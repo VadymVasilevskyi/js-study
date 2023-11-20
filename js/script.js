7e4ff972834d114396f3ac05fe6cbbcc28c18712
VadymVasilevskyi <vadim.cryptoru@gmail.com>
2023-11-20 09:38:20 +0100

    turn func into objMethods

--- js/script.js
+++ js/script.js
@@ -1,83 +1,70 @@
+
 'use strict'
 
-let numberOfFilms;
 
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-
-start();
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFilms: function() {
+
+        for (let i = 0; i < 2; i++) {
+            const  a = prompt ('Один из просмотренных фильмов', ''),
+                   b = prompt ('На сколько оцените его', '');
+        
+            if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+            
+        //Если а не равно 'Отмена' b не равно "Отмена" И а не равно "пустая строка"
+        // b не равно " пустая строка" и длина а меньше 50 символов
+                        personalMovieDB.movies[a] = b;
+                    console.log ('done');
+            } else {
+                    console.log('error');
+                    i--;
+                }
+                   
+        
+        }
+    },
+    detectPersonalLevel: function () {
+
+        if (personalMovieDB.count < 10) {
+            console.log ('Просмотрено довольно мало фильмов');
+        } else if (personalMovieDB.count >=10 && personalMovieDB.count < 30) {
+            console.log('Вы классический зритель');
+        } else if (personalMovieDB.count >= 30) {
+            console.log('Вы киноман');
+        } else {
+            console.log ('error');
+        }
+    },
+
+    showMyDB: function (hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres: function () {
+        for (let i =1; i < 4; i++) {
+             
+            personalMovieDB.genres[i-1] = prompt (`Ваш любимый жанр под номером ${i}`); // Если не использовать индекс - 1 в базу данных элемент под индексом 0 запишется как пустой объект
+        }
+    }
 };
 
 
 
 
 
-function rememberMyFilms() {
-
-    for (let i = 0; i < 2; i++) {
-        const  a = prompt ('Один из просмотренных фильмов', ''),
-               b = prompt ('На сколько оцените его', '');
-    
-        if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
-        
-    //Если а не равно 'Отмена' b не равно "Отмена" И а не равно "пустая строка"
-    // b не равно " пустая строка" и длина а меньше 50 символов
-                    personalMovieDB.movies[a] = b;
-                console.log ('done');
-        } else {
-                console.log('error');
-                i--;
-            }
-               
-    
-    }
-}
-
-rememberMyFilms ();
 
 
-
-function detectPersonalLevel () {
-
-    if (personalMovieDB.count < 10) {
-        console.log ('Просмотрено довольно мало фильмов');
-    } else if (personalMovieDB.count >=10 && personalMovieDB.count < 30) {
-        console.log('Вы классический зритель');
-    } else if (personalMovieDB.count >= 30) {
-        console.log('Вы киноман');
-    } else {
-        console.log ('error');
-    }
-}
-
-detectPersonalLevel();
-
-function showMyDB (hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.privat);
-
-
-function writeYourGenres () {
-    for (let i =1; i < 4; i++) {
-         
-        personalMovieDB.genres[i-1] = prompt (`Ваш любимый жанр под номером ${i}`); // Если не использовать индекс - 1 в базу данных элемент под индексом 0 запишется как пустой объект
-    }
-}
-
-writeYourGenres();
